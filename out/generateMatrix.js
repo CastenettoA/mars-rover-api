51a905463833ef67648e26b05d6ba422215dda1d
castenettoa <castenetto.andrea@gmail.com>
2022-11-15 18:10:03 +0100

    Server, Router, Controllers, API logic

--- out/generateMatrix.js
+++
@@ -1,9 +0,0 @@
-// let map: Array<Array<number>> = []; //// it's the same
-var map = [];
-var mapLength = 4;
-for (var i = 0; i <= mapLength; i++) {
-    map.push([0, i]);
-    for (var inner = 1; inner <= mapLength; inner++) {
-        map.push([inner, i]);
-    }
-}
--- out/routes/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// https://www.section.io/engineering-education/how-to-create-a-simple-rest-api-using-typescript-and-nodejs/
-var controllers_1 = require("../controllers/");
-var router = express.Router();
-router.get('/rover', controllers_1.default.initRover);
-router.put('/move', controllers_1.default.moveRover);
-// more easy implementation (vanilla)
-// https://www.topcoder.com/thrive/articles/how-to-build-rest-apis-with-typescript-with-no-frameworks-and-only-using
