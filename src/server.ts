d6fe0d87f4bfeb1ae3df9e506e574a18083713dd
castenettoa <castenetto.andrea@gmail.com>
2022-11-22 10:46:23 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -24,17 +24,17 @@ app.use(express.json());
 /* Enable All CORS request (Cross Origin Resource Sharing) */
 app.use(cors());
 
-/** RULES OF OUR API */
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");   // set the CORS policy
-  res.header("Access-Control-Allow-Headers","origin, X-Requested-With,Content-Type,Accept, Authorization"); // set the CORS headers
-  // set the CORS method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-    return res.status(200).json({});
-  }
-  next();
-});
+// /** RULES OF OUR API */
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");   // set the CORS policy
+//   res.header("Access-Control-Allow-Headers","origin, X-Requested-With,Content-Type,Accept, Authorization"); // set the CORS headers
+//   // set the CORS method headers
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.status(200).render("index", {
