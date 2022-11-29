5c1e370a88ae7ff999da8b11d703d5646a85062c
castenettoa <castenetto.andrea@gmail.com>
2022-11-29 08:46:38 +0100

    fixed untracked files

--- build/routes/rover.js
+++
@@ -1,7 +0,0 @@
-// // this file connect the route to their controller
-// import express from 'express';
-// import controller from '../controllers/rover_old';
-// const router = express.Router();
-// router.get('/initRover', controller.initRover);
-// router.put('/moveRover/:commands', controller.moveRover);
-// export = router;
